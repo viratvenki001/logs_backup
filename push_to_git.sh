ff8167478c07252c78c5e3200ec54959a8294ecc
root <root@ip-172-31-29-33.eu-north-1.compute.internal>
2025-07-14 14:46:52 +0000

    Daily log update

---
+++ push_to_git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set log directory and Git repository directory
+LOG_DIR=/var/logs/app
+GIT_REPO=/var/logs/app/log-repo
+
+# Change to the Git repository directory
+cd $GIT_REPO
+
+# Add all changes
+git add .
+
+# Commit changes
+git commit -m "Daily log update"
+
+# Push changes to the remote repository
+git push origin master
